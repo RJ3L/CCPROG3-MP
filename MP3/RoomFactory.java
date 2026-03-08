f468820c5ab4dfb4fe4da0e7a3941dbcc46e1d0d
Richard Limtin <jeremylimtin@gmail.com>
2026-03-09 00:38:17 +0800

    fix: create hotel

--- MP3/RoomFactory.java
+++ MP3/RoomFactory.java
@@ -3,9 +3,9 @@ package MP3;
 public class RoomFactory {
     public static RoomInterface createRoom(String type, String name, Hotel hotel) {
         return switch (type.toLowerCase()) {
-            case "Standard" -> new StandardRoom(name, hotel);
-            case "Deluxe" -> new DeluxeRoom(name, hotel);
-            case "Luxury" -> new LuxuryRoom(name, hotel);
+            case "standard" -> new StandardRoom(name, hotel);
+            case "deluxe" -> new DeluxeRoom(name, hotel);
+            case "luxury" -> new LuxuryRoom(name, hotel);
             default -> throw new IllegalArgumentException("Unknown room type: " + type);
         };
     }
